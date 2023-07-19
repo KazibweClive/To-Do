586060308423ba7d8a9343ea25467104dece7184
KazibweClive <clivekazibwe@gmail.com>
2023-07-19 17:31:07 +0300

    fix: tasks to an array

--- index.js
+++ index.js
@@ -1,149 +1,152 @@
 let taskInput = document.querySelector('input');
 let button = document.querySelector('button');
 let taskContainer = document.querySelector('#task-container');
-let tasks;
+let tasks = [];
 let toDo;
 let myStoredTasks;
+// localStorage.clear() -- Only when necessary
 //# Save task to local Storage
-function saveTaskToLocalStorage() {
+function saveTaskToLocalStorage(){
     let storedData = getDataFromLocalStorage();
-
-    if (storedData === null) {
-
+    
+    if(storedData === null){
+        
         tasks.push(taskInput.value)
         toDo = JSON.stringify(tasks);
-
+        
         localStorage.setItem('task', toDo);
 
-    } else {
+    }else{
         tasks = JSON.parse(storedData)
         tasks.push(taskInput.value);
 
         toDo = JSON.stringify(tasks);
         localStorage.setItem('task', toDo)
-    }
+    }    
 }
 // # Load Tasks from localStorage
-function getDataFromLocalStorage() {
+function getDataFromLocalStorage(){
     let myTasks = localStorage.getItem('task')
-    if (!myTasks) {
+    if(!myTasks){
         return null
-    } else {
+    }else{
         return myTasks;
     }
 }
 
-function viewTasks() {
+function viewTasks(){
     myStoredTasks = getDataFromLocalStorage();
-
-    if (myStoredTasks === null) {
+    
+    if(myStoredTasks === null){
         //optional--display no tasks yet
-    } else {
-        taskContainer.innerHTML = "";
+    }else{
+        taskContainer.innerHTML="";
         myStoredTasks = JSON.parse(myStoredTasks);
-        myStoredTasks.forEach((taskDescription, index) => {
+        myStoredTasks.forEach((taskDescription,index) =>{
             let listItem = document.createElement('li')
-            let task = `<span>${taskDescription}</span>&nbsp;<button class='edit' value=${index}>edit</button>&nbsp;<button class="delete" value='${taskDescription}'>delete</button>`
+            let task = `<span>${taskDescription}</span>&nbsp;<button class='fas fa-edit edit' value=${index}></button>&nbsp;<button class="fa fa-trash delete" value='${taskDescription}'></button>`
             listItem.innerHTML = task;
-
+            
             taskContainer.appendChild(listItem)
 
             editTask();
             deleteTask();
-
+            
         })
     }
-
+    
 }
 
-button.addEventListener('click', function (e) {
+button.addEventListener('click', function(e){
     e.preventDefault();
-    if (taskInput.value !== "") {
+    if(taskInput.value !== ""){
         saveTaskToLocalStorage();
-        taskInput.value = "";
+        taskInput.value="";
         taskInput.setAttribute('placeholder', 'Enter a Task')
     }
-
+    
     viewTasks();
 })
 viewTasks();
 
-function editTask() {
-    document.querySelectorAll('.edit').forEach(taskItem => {
-        taskItem.addEventListener('click', function (e) {
+function editTask(){
+    document.querySelectorAll('.edit').forEach(taskItem =>{
+        taskItem.addEventListener('click',function(e){
             e.preventDefault();
             myStoredTasks = getDataFromLocalStorage();
-            if (myStoredTasks) {
+            if(myStoredTasks){
                 myStoredTasks = JSON.parse(myStoredTasks);
                 let form = document.createElement('form');
                 form.setAttribute('id', 'update')
                 updateTask = document.createElement('input')
-                updateTask.setAttribute('placeholder', `${myStoredTasks[e.target.value]}`)
+                updateTask.setAttribute('placeholder',`${myStoredTasks[e.target.value]}`)
                 let submitButton = document.createElement('button');
                 submitButton.innerText = "Update";
-                submitButton.setAttribute('value', e.target.value)
+                submitButton.setAttribute('value', e.target.value )
                 form.appendChild(updateTask)
                 form.appendChild(submitButton)
                 let editContainer = document.querySelector('#edit-container')
                 editContainer.replaceChildren();
                 editContainer.appendChild(form);
-
-
-
-                let updateForm = document.querySelector('#update');
-
-                updateForm.addEventListener('click', function (e) {
-                    e.preventDefault();
-                    if (updateTask.value !== "") {
-                        myStoredTasks = JSON.parse(getDataFromLocalStorage());
-                        //update the value in the array
-                        myStoredTasks[e.target.value] = updateTask.value;
-
-                        myStoredTasks = JSON.stringify(myStoredTasks);
-
-                        localStorage.setItem('task', myStoredTasks);
-
-                        viewTasks();
-                        editContainer.replaceChildren();
-                    }
-
-                })
+    
+    
+    
+            let updateForm = document.querySelector('#update');
+            
+            updateForm.addEventListener('click',function(e){
+                e.preventDefault();
+                if(updateTask.value !== ""){
+                    myStoredTasks = JSON.parse(getDataFromLocalStorage());
+                    //update the value in the array
+                    myStoredTasks[e.target.value] = updateTask.value;
+    
+                    myStoredTasks = JSON.stringify(myStoredTasks);
+    
+                    localStorage.setItem('task' , myStoredTasks);
+    
+                    viewTasks();
+                    editContainer.replaceChildren();
+                }
+                
+            })
             }
-
+             
         })
-    })
+     })
 }
 
-function deleteTask() {
+function deleteTask(){
+    
+    document.querySelectorAll('.delete').forEach(taskItem =>{
 
-    document.querySelectorAll('.delete').forEach(taskItem => {
-
-        taskItem.addEventListener('click', function (e) {
+        taskItem.addEventListener('click',function(e){
             e.preventDefault();
 
             myStoredTasks = JSON.parse(getDataFromLocalStorage());
-
+            
             let index = myStoredTasks.indexOf(e.target.value)
-            if (index !== -1) {
+            if(index !== -1){
+
                 // perform the delete action
-                myStoredTasks.splice(index, 1);
+                myStoredTasks.splice(index,1);
 
                 myStoredTasks = JSON.stringify(myStoredTasks);
-
+                
                 localStorage.setItem('task', myStoredTasks);
 
             }
-
-
+           
+            
             viewTasks();
-
+            
         })
-
+        
     })
-
+   
 }
 
+//deleteTask()
 
 
 
-
+  
